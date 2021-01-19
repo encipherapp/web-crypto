6d7c552a69e745ed420a894154e514835f304453
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-01-19 23:41:29 +0530

    added babel + webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webCrypto.js',
+    library: 'webCrypto',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+};
