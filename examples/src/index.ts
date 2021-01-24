95557226f4a94cb50193ce71476edcf9a0d3c730
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-01-25 02:59:11 +0530

    WIP

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,3 +1,45 @@
-console.log('Hello World');
+import { unwrapKeyWithAesCbc256 } from '@/services/keyGeneration/keyGeneration';
+import {
+  wrapKeyWithAesCbc256,
+  KeyGenerator,
+  KeyGeneratorMaps,
+} from '../../src';
 
-export default null;
+const algorithm: RsaHashedKeyGenParams = {
+  name: KeyGeneratorMaps.rsaKeyGenerationAlgorithmsMap['RSA-OAEP'],
+  modulusLength: 4096,
+  publicExponent: new Uint8Array([1, 0, 1]),
+  hash: KeyGeneratorMaps.rsaKeyHashFunctionsMap['SHA-256'],
+};
+
+const keyGen = new KeyGenerator(algorithm);
+let rsaKeyPair: CryptoKeyPair;
+
+(keyGen.generateKey() as Promise<CryptoKeyPair>)
+  .then(async (data) => {
+    rsaKeyPair = data;
+    const wrappedKey = await wrapKeyWithAesCbc256(
+      'shirshendu',
+      rsaKeyPair.privateKey,
+    );
+    console.log(wrappedKey);
+    const unwrappedKey = await unwrapKeyWithAesCbc256('shirshendu', wrappedKey);
+
+    console.log(rsaKeyPair.privateKey);
+    console.log(unwrappedKey);
+
+    const originalKey = await window.crypto.subtle.exportKey(
+      'pkcs8',
+      rsaKeyPair.privateKey,
+    );
+    const decryptedKey = await window.crypto.subtle.exportKey(
+      'pkcs8',
+      unwrappedKey,
+    );
+
+    console.log(originalKey);
+    console.log(decryptedKey);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
