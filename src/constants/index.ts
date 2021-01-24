114430faf276d303a1d503b78d4bf6c5a7242ec5
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-01-24 20:13:07 +0530

    modfied default values

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const DefaultKeyUsage: KeyUsage[] = [
-  'encrypt',
-  'decrypt',
-  'sign',
-  'verify',
-  'wrapKey',
-  'unwrapKey',
-];
+export const DefaultKeyUsage: KeyUsage[] = ['encrypt', 'decrypt'];
