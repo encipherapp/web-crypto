81307527b7b3952a90e3c81f090be896aeed0b21
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-02-02 01:59:37 +0530

    added array buffer to string conversion and vice versa

--- src/constructors/ExportedKey/ExportedKey.ts
+++ src/constructors/ExportedKey/ExportedKey.ts
@@ -26,20 +26,26 @@ class ExportedKey {
   wrappingInfo: NonWrappedInfo | WrappedInfo;
 
   constructor(
-    key: ArrayBuffer,
+    key: ArrayBuffer | string,
     algorithm: CryptoKey['algorithm'],
     type: KeyType,
     usages: KeyUsage[],
     wrappingInfo: NonWrappedInfo | WrappedInfo,
   ) {
-    this.key = key;
+    this.key = typeof key === 'string' ? ExportedKey.toByteArray(key) : key;
     this.algorithm = algorithm;
     this.type = type;
     this.usages = usages;
     this.wrappingInfo = wrappingInfo;
   }
 
-  // static toByteArray(hexString) {}
+  static toByteArray(hexString: string): ArrayBuffer {
+    const byteArray = new Uint8Array(hexString.length / 2);
+    for (let i = 0; i < hexString.length; i += 2) {
+      byteArray[i / 2] = parseInt(hexString.substr(i, 2), 16);
+    }
+    return byteArray.buffer;
+  }
 
   toHexString(): string {
     const byteArray = new Uint8Array(this.key);
