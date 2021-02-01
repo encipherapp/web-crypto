68a7c6cc3a35a2b01da7ef2916c1de64c08aa858
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-02-02 02:38:33 +0530

    refactor

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,6 @@
-import {
-  generatePbkdf2FromPassphrase,
-  deriveKey,
-  wrapKeyWithAesCbc256,
-  unwrapKeyWithAesCbc256,
-} from './keyGeneration/keyGeneration';
-
 export {
   generatePbkdf2FromPassphrase,
   deriveKey,
   wrapKeyWithAesCbc256,
   unwrapKeyWithAesCbc256,
-};
+} from './keyGeneration/keyGeneration';
